c158aabfc6b0a0e17a7101d9e7ddd307e5b8be85
Matt Herrick <matthew@duplocloud.net>
2024-02-15 14:58:06 -0600

    making version constraints use minimum versions

--- modules/tenant-role-extension/versions.tf
+++ modules/tenant-role-extension/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.12.0"
+      version = ">= 5.12.0"
     }
     duplocloud = {
       source  = "duplocloud/duplocloud"
-      version = "> 0.9.40"
+      version = ">= 0.9.40"
     }
   }
 }
