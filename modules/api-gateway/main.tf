a40f3bb6484b9fb5721fa8c9686d03f5b004d120
Kelly Ferrone <kelly@duplocloud.net>
2024-08-01 15:07:03 -0600

    more typos

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -4,9 +4,9 @@ locals {
   fullname    = "duplo-${local.shortname}"
   namespace   = "duploservices-${var.tenant_name}"
   sg_infra    = tolist(data.duplocloud_infrastructure.this.security_groups)
-  base_domain = data.duplocloud_plan_settings.this.dns_setting.external_dns_suffix
+  base_domain = data.duplocloud_plan_settings.this.dns_setting[0].external_dns_suffix
   domain      = "${local.shortname}.${local.base_domain}"
-  zone_id     = data.duplocloud_plan_settings.this.dns_setting.domain_id
+  zone_id     = data.duplocloud_plan_settings.this.dns_setting[0].domain_id
   base_tags = {
     TENANT_NAME   = var.tenant_name
     duplo-project = var.tenant_name
@@ -32,4 +32,8 @@ data "duplocloud_plan_settings" "this" {
 data "duplocloud_plan_certificate" "this" {
   name    = var.cert_name
   plan_id = data.duplocloud_tenant.this.plan_id
+}
+
+data "duplocloud_tenant_internal_subnets" "this" {
+  tenant_id = local.tenant_id
 }
