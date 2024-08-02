6bc094b2ace884bdc39bc1a2dc05d8a3ebbfefe7
Kelly Ferrone <kelly@duplocloud.net>
2024-08-02 14:37:30 -0600

    slim the lambda
    Co-authored-by: Vishaal Chhabria <Vchabs@users.noreply.github.com>

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,7 +1,5 @@
 locals {
   tenant_id  = data.duplocloud_tenant.this.id
-  account_id = data.duplocloud_aws_account.this.account_id
-  region     = data.duplocloud_tenant_aws_region.this.aws_region
 }
 
 data "duplocloud_tenant" "this" {
@@ -10,10 +8,4 @@ data "duplocloud_tenant" "this" {
 
 data "duplocloud_infrastructure" "this" {
   tenant_id = data.duplocloud_tenant.this.id
-}
-
-data "duplocloud_aws_account" "this" {}
-
-data "duplocloud_tenant_aws_region" "this" {
-  tenant_id = local.tenant_id
 }
