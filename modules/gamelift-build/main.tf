68378db2e691a69cdd484b44ce85d37e51ff941d
Kelly Ferrone <kelly@duplocloud.net>
2024-08-28 15:34:22 -0600

    tenant in name

--- modules/gamelift-build/main.tf
+++ modules/gamelift-build/main.tf
@@ -2,7 +2,7 @@ locals {
   account_id             = data.duplocloud_aws_account.this.account_id
   tenant_prefix          = "duploservices-${var.tenant_name}"
   tenant_role_arn        = "arn:aws:iam::${local.account_id}:role/${local.tenant_prefix}"
-  build_name             = "${var.name}-${var.build.version}"
+  build_name             = "${var.tenant_name}-${var.name}-${var.build.version}"
   bucket_tenant          = var.bucket.bucket_tenant_name
   bucket_tenant_prefix   = "duploservices-${local.bucket_tenant}"
   bucket_tenant_role_arn = "arn:aws:iam::${local.account_id}:role/${local.bucket_tenant_prefix}"
