541efbd8cc4e380029fd5abe2c4a1098ad56992b
Kelly Ferrone <kelly@duplocloud.net>
2024-08-28 15:57:04 -0600

    output issues

--- modules/gamelift-build/outputs.tf
+++ modules/gamelift-build/outputs.tf
@@ -1,16 +1,7 @@
 output "build_id" {
   value = awscc_gamelift_build.this.id
-  
 }
 
-output "fleet_arn" {
-  value = "arn:aws:gamelift:${local.region}:${local.account_id}:fleet/${awscc_gamelift_fleet.this.fleet_id}"
-}
-
-output "dm_fleet_arn" {
-  value = "arn:aws:gamelift:${local.region}:${local.account_id}:fleet/${awscc_gamelift_fleet.this.fleet_id}"
-}
-
-output "squads_fleet_arn" {
-  value = "arn:aws:gamelift:${local.region}:${local.account_id}:fleet/${awscc_gamelift_fleet.this.fleet_id}"
+output "fleet_id" {
+  value = awscc_gamelift_fleet.this.fleet_id
 }
