cb19e345beeba70a4319e2055160dbe3b270f435
Vishaal Chhabria <vishaal@duplocloud.net>
2024-08-30 19:44:08 -0400

    updated some stuff

--- modules/ssm/ssm.tf
+++ modules/ssm/ssm.tf
@@ -1,8 +1,8 @@
 resource "duplocloud_aws_ssm_parameter" "ssm_param" {
   count = length(var.parameters)
 
-  tenant_id = duplocloud_tenant.myapp.tenant_id
-  name      = var.parameters[count.index].name
+  tenant_id = local.tenant_id
+  name      = "${local.tenant_name}/${var.parameters[count.index].name}"
   type      = coalesce(var.parameters[count.index].type, "SecureString")
   value     = var.parameters[count.index].value
 }
